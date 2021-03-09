aaed628cee276343cff32c88d33f507c402f362e
Jonathas <master.donte@hotmail.com>
2021-03-08 23:31:57 -0300

    Testes iniciais

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+class App extends React.Component{
+
+  state = {
+    numero1 : '',
+    numero2 : '',
+    resultado : ''
+  }
+
+  somar = () => {
+    const resultado = parseInt(this.state.numero1) + parseInt(this.state.numero2);
+    this.setState({resultado: resultado});
+  }
+
+  render(){
+    return(
+      <div>
+        <label>Digite o numero 1</label>
+        <input type="text" value={this.state.numero1} onChange={(e) => this.setState({numero1 : e.target.value}) }/>
+        <br/>
+        <label>Digite o numero 2</label>
+        <input type="text" value={this.state.numero2} onChange={(e) => this.setState({numero2 : e.target.value}) }/>
+        <br/>
+        <button onClick={this.somar}>Soma</button>
+        <br/>
+        O resultado da soma é {this.state.resultado}
+      </div>
+    );
+  }
+}
+
+/*
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hello world!!
     </div>
   );
 }
-
+*/
 export default App;
